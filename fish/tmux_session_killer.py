fd871a3a708f55323bb0b98140c532aa3583c2fa
maestro-bene <gael.cudennec@gmail.com>
2024-03-01 14:12:50 +0100

    feat(fish): tmux script

--- fish/tmux_session_killer.py
+++ fish/tmux_session_killer.py
@@ -8,18 +8,15 @@ def main():
             ["/opt/homebrew/bin/tmux", "ls"], universal_newlines=True
         )
         lines = output.strip().split("\n")
-        temp_exists_already = False
         for line in lines:
             session_name = line.split(":")[0]
             if session_name == "temp":
-                temp_exists_already = True
                 subprocess.run(["tmux", "a", "-t", session_name])
             if session_name.isdigit():
                 subprocess.run(["tmux", "kill-ses", "-t", session_name])
-        if not temp_exists_already:
-            subprocess.run(["tmux", "new", "-s", "temp"])
     except subprocess.CalledProcessError:
         print("tmux is not running or some other error occurred")
+        subprocess.run(["tmux", "new", "-s", "temp"])
 
 
 if __name__ == "__main__":
